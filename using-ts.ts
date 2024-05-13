09f96b72a8db75a90eb1beb4d430e651d11fc9ee
kashyup <rohitkashyap69489@gmail.com>
2024-05-13 18:42:54 +0530

    variable changes

--- using-ts.ts
+++ using-ts.ts
@@ -1,6 +1,6 @@
-var button = document.querySelector('button');
-var input1 = document.getElementById('num1')! as HTMLInputElement;
-var input2 = document.getElementById('num2')! as HTMLInputElement;
+const button = document.querySelector('button');
+const input1 = document.getElementById('num1')! as HTMLInputElement;
+const input2 = document.getElementById('num2')! as HTMLInputElement;
 
 function add(num1: number, num2: number) {
     return num1 + num2;
